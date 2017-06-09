bbb91e81ab7845e146624f34ef1363def6f5ae45
mi-24v <miwpayou0808@gmail.com>
2017-06-09 22:22:58 +0900

    initial Android files

---
+++ app/src/main/java/info/miwpayou0808/imacoconow/MainActivity.kt
@@ -0,0 +1,12 @@
+package info.miwpayou0808.imacoconow
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
